8015fc4b3db4f8d54c9fd14b02ca6d352474b708
Mgcsuper <matgio.caroli@gmail.com>
2025-06-16 18:39:13 +0200

    dans work_matteo.ipynb, il y a les 3 première étapes de la pipline au plus simple, ensuite on visualise avant et après le rafinage. Dans refinement.py les fonctions permettant de rafiner une courbe, devide renvoie une liste d'indice de coupure, et refine utilise dvide pour couper chaque edge du graphe. Dans graphe_to_bezier de simple fonction bezier_from_graphe

---
+++ src/bez/graph_to_bezier.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from bez.bezier import fit_bezier, interpolate_bezier
+
+def bezier_from_graph(topo_graph):
+    bezier_fits = []
+    for i,edge in enumerate(topo_graph.edges(data=True, keys=True)):
+        u, v,key, data = edge
+        pixels = data["pixels"]
+        if len(pixels) < 3:
+            continue
+        # Fit a Bezier curve to the pixel line
+        p = np.vstack((pixels[:, 0], pixels[:, 1]))
+        instants = np.linspace(0, 1, p.shape[1])
+        control_points = fit_bezier(p,instants)
+        bezier_fits.append(((u,v, key),control_points))
+
+    return bezier_fits
+
+
+def visualize_bezier_fits(img, bezier_fits):
+    plt.figure(figsize=(8, 8))
+    # Superpose l'image originale
+    plt.imshow(img, cmap="gray", alpha=0.2)
+    for _, control_points in bezier_fits:
+        t = np.linspace(0, 1, 100)
+        bezier_curve = interpolate_bezier(control_points, t)
+        plt.plot(bezier_curve[1], bezier_curve[0], color='red', linewidth=1)
+    plt.axis("equal")
+    plt.title("Bezier Fits on Topological Graph (superposé à l'image originale)")
+    plt.show()
---
+++ src/bez/refinement.py
@@ -0,0 +1,117 @@
+import networkx as nx
+import numpy as np
+from typing import List, Optional
+
+from bez.bezier import fit_bezier, interpolate_bezier, std_distance
+
+
+
+def fit_bezier_error(pixel_c : np.array):
+    """
+    arg :
+        pixel_c (nd.array): 2 x N
+    """
+    assert pixel_c.shape[0] == 2
+
+    instants = np.linspace(0, 1, pixel_c.shape[1])
+    
+    control_points = fit_bezier(pixel_c, instants)
+    traj_fit = interpolate_bezier(control_points, instants)
+
+    error = std_distance(traj_fit, pixel_c)
+
+    return control_points, error
+
+
+
+
+def fit_bezier_error_T(pixel_c : np.array):
+    """
+    arg :
+        pixel_c (nd.array): N x 2
+    """
+    assert pixel_c.shape[1] == 2
+    
+    traj = pixel_c.T
+    return fit_bezier_error(traj)
+
+
+
+def devide(pixel_c: np.ndarray, precision: float = 2.0) -> List[int]:
+    _, e = fit_bezier_error_T(pixel_c)
+    if e <= precision:
+        return np.array([])
+
+    N = len(pixel_c)
+
+    _, e1 = fit_bezier_error_T(pixel_c[:N // 3])
+    _, e2 = fit_bezier_error_T(pixel_c[N // 3:])
+    _, e3 = fit_bezier_error_T(pixel_c[:2 * N // 3])
+    _, e4 = fit_bezier_error_T(pixel_c[2 * N // 3:])
+
+    if e1 + e2 < e3 + e4:
+        first = devide(pixel_c[:N//3])
+        second = devide(pixel_c[N//3:])
+        idx = np.array([N//3])
+
+        if first.size == 0:
+            first = np.array([])
+        if second.size == 0:
+            second = np.array([])
+        else:
+            second = second + idx  # ajout de idx à chaque élément
+
+        return np.concatenate([first, idx, second])
+    
+    else : 
+        first = devide(pixel_c[:2*N//3])
+        second = devide(pixel_c[2*N//3:])
+        idx = np.array([2*N//3])
+
+        if first.size == 0:
+            first = np.array([])
+        if second.size == 0:
+            second = np.array([])
+        else:
+            second = second + idx  # ajout de idx à chaque élément
+
+        return np.concatenate([first, idx, second])
+
+
+
+
+
+
+def refine(topo_g: nx.MultiDiGraph, precision: float = 2.0):
+    new_topo_graph = nx.MultiDiGraph()
+
+    for u, v, data in topo_g.edges(data=True):
+        pixels = data["pixels"]
+
+        if pixels.shape[0] < 3 :
+            continue
+
+        indices = devide(pixels, precision)
+
+        if indices is None or len(indices) == 0:
+            # Rien à changer
+            new_topo_graph.add_node(u)
+            new_topo_graph.add_node(v)
+            new_topo_graph.add_edge(u, v, pixels = pixels)
+            continue
+
+
+        # Créer les nouveaux points d'interpolation
+        indices = list(indices.astype(int))
+        cuts = [0] + indices + [len(pixels)-1]
+
+        
+        for i in range(len(cuts)-1):
+            node_a = tuple(map(int, pixels[cuts[i]]))
+            node_b = tuple(map(int, pixels[cuts[i+1]]))
+            new_topo_graph.add_node(node_a)
+            new_topo_graph.add_node(node_b)
+            new_topo_graph.add_edge(node_a, node_b, pixels = pixels[cuts[i]:cuts[i+1]])
+
+    # Remplacer topo_c par les courbes mises à jour
+    return new_topo_graph
