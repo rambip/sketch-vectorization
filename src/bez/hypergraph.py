de49e0019218dea82adea9162d7077f0f24ebc0d
rambip <apero1808@gmail.com>
2025-06-16 10:03:00 +0200

    Improved skeleton for hypergraph optimization

--- src/bez/hypergraph.py
+++ src/bez/hypergraph.py
@@ -1,5 +1,27 @@
+import polars as pl
+import networkx as nx
 import numpy as np
 from dataclasses import dataclass
+
+
+INCREASE_DEGREE = 0
+DECREASE_DEGREE = 1
+MERGE = 2
+SPLIT = 3
+OVERLAP = 4
+DISSOCIATE = 5
+
+CHOICE_DISTRIBUTION = [
+    # change degree
+    1 / 6,
+    1 / 6,
+    # merge or split
+    1 / 6,
+    1 / 6,
+    # overlap or dissociate
+    1 / 6,
+    1 / 6,
+]
 
 
 @dataclass
@@ -9,60 +31,134 @@ class HyperEdgeIndex:
 
 
 class HyperGraph:
-    def __init__(self, size: int, edges):
-        # TODO: degrees
-        self.hedges = []
-        for a, b in edges:
-            self.hedges.append([a, b])
-        self.tip_counts = np.zeros((1,))
-
-    def update_counts(self):
-        self.tip_counts.fill(0)
-        for h in self.hedges:
-            if len(h) == 1:
-                self.tip_counts[h[0]] += 1
-            first = h[0]
-            last = h[-1]
-            self.tip_counts[first] += 1
-            self.tip_counts[last] += 1
+    def __init__(self, topo_graph: nx.MultiGraph):
+        self.data = pl.DataFrame(
+            [
+                {"nodes": [u, v], "edges": [key], "degree": 3, "score": None}
+                for u, v, key, weight in topo_graph.edges(keys=True)
+            ]
+        ).with_row_index()
+        self.topo = topo_graph
 
     def get_hyperedge(self, x: HyperEdgeIndex):
-        result = self.hedges[x.i]
+        nodes = self.data[x.i, "nodes"]
+        edges = self.data[x.i, "edges"]
         if x.reverse:
-            return result[::-1]
-        return result
+            return (nodes[::-1], edges[::-1])
+        return (nodes, edges)
 
     def merge(self, a: HyperEdgeIndex, b: HyperEdgeIndex):
-        ha = self.get_hyperedge(b)
-        hb = self.get_hyperedge(a)
-        assert ha[-1] == hb[0]
-        new = ha[:-1] + hb[1:]
-        self.hedges[a.i] = new
-        self.hedges[b.i] = self.hedges.pop()
+        na, ea = self.get_hyperedge(b)
+        nb, eb = self.get_hyperedge(a)
+        assert na[-1] == nb[0]
+        new_nodes = na[:-1] + nb[1:]
+        new_edges = ea + eb
+        self.data = self.data.remove(
+            (pl.col("index") == a.i) | (pl.col("index") == b.i)
+        ).extend(
+            pl.DataFrame(
+                {
+                    "nodes": [new_nodes],
+                    "edges": [new_edges],
+                    "score": [None],
+                }
+            )
+        )
 
     def split(self, a: HyperEdgeIndex, node: int):
-        ha = self.get_hyperedge(a)
-        j = ha.index(node)
-        result1 = ha[: j + 1]
-        result2 = ha[j:]
-        self.hedges[a.i] = result1
-        self.hedges.append(result2)
+        na, ea = self.get_hyperedge(a)
+        j = na.index(node)
+        n1 = na[: j + 1]
+        n2 = na[j:]
+        e1 = ea[:j]
+        e2 = ea[j:]
+        self.data = self.data.remove((pl.col("index") == a.i)).extend(
+            pl.DataFrame(
+                {
+                    "nodes": [n1, n2],
+                    "edges": [e1, e2],
+                    "score": [None, None],
+                }
+            )
+        )
 
     def overlap(self, a: HyperEdgeIndex, b: HyperEdgeIndex):
-        ha = self.get_hyperedge(b)
-        hb = self.get_hyperedge(a)
-        assert ha[-1] == hb[0]
-        assert len(hb) >= 2
-        self.hedges[a.i] = ha + [hb[1]]
+        na, ea = self.get_hyperedge(b)
+        nb, eb = self.get_hyperedge(a)
+        assert na[-1] == nb[0]
+        assert len(nb) >= 2
+        self.data
+        self.data = self.data.remove((pl.col("index") == a.i)).extend(
+            pl.DataFrame(
+                {
+                    "nodes": [na + [nb[1]]],
+                    "edges": [ea + eb[0]],
+                    "score": [None],
+                }
+            )
+        )
 
     def dissociate(self, a: HyperEdgeIndex, b: HyperEdgeIndex):
-        ha = self.get_hyperedge(b)
-        hb = self.get_hyperedge(a)
-        assert ha[-2] == hb[0]
-        assert ha[-1] == hb[1]
-        self.hedges[a.i] = ha[:-1]
+        na, ea = self.get_hyperedge(b)
+        nb, eb = self.get_hyperedge(a)
+        assert na[-2] == nb[0]
+        assert na[-1] == nb[1]
+        assert ea[-1] == eb[0]
+        self.data = self.data.remove((pl.col("index") == a.i)).extend(
+            pl.DataFrame(
+                {
+                    "nodes": [na[:-1]],
+                    "edges": [ea[:-1]],
+                    "score": [None],
+                }
+            )
+        )
+
+    def increase_degree(self, i: int):
+        d = self.data[i, "degree"]
+        assert d == 2
+        self.data[i, "degree"] = 3
+        self.data[i, "score"] = None
+
+    def decrease_degree(self, i: int):
+        d = self.data[i, "degree"]
+        assert d == 3
+        self.data[i, "degree"] = 2
+        self.data[i, "score"] = None
+
+    def random_merge(self):
+        """
+        To do a random merge, we need to find one node A such that there are 2 hyperedges with extremity A
+        """
+
+    def random_split(self):
+        """
+        To do a random split, we need to find any hyperedge containing at least 3 nodes
+        """
+
+    def random_overlap(self):
+        """
+        To do a ranodm overlap, we need to find 2 hyperedges U, V such that U is completely contained in V
+        """
+
+    def random_dissociate(self):
+        """
+        To do a random dissociation, we need to find 2 hyperedges such that U is completely contained in V
+        """
 
     def random_perturbation(self):
-        # Problem: we want to evaluate the energy of the new config.
-        # Is it a good idea to generate the perturbation, evaluate and go back ?
-        ...
+        choice = np.random.choice(range(3), p=CHOICE_DISTRIBUTION)
+        if choice == INCREASE_DEGREE:
+            i_h = np.random.choice(self.data.index)
+            self.increase_degree(i_h)
+        if choice == DECREASE_DEGREE:
+            i_h = np.random.choice(self.data.index)
+            self.decrease_degree(i_h)
+        if choice == OVERLAP:
+            self.random_overlap()
+        if choice == DISSOCIATE:
+            self.random_dissociate()
+        if choice == MERGE:
+            self.random_merge()
+        if choice == SPLIT:
+            self.random_split()
