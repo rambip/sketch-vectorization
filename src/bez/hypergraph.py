37a364892ba2184d86799a293ada27b65f7a0924
rambip <apero1808@gmail.com>
2025-06-16 16:37:35 +0200

    simplification: triplets (u, v, k) pour les noeuds

--- src/bez/hypergraph.py
+++ src/bez/hypergraph.py
@@ -29,8 +29,6 @@ NODE = 2
 @dataclass
 class HyperEdge:
     degree: int
-    start: int
-    end: int
     score: float | None
     edges: list[int]
 
@@ -38,81 +36,64 @@ class HyperEdge:
 class HyperGraph:
     def __init__(self, topo_graph: nx.MultiGraph):
         self.counter = itertools.count(start=0)
-        self.tripartite = nx.DiGraph()
+        self.hyper2edge = nx.DiGraph()
+        self.topo = topo_graph
+        self.table = dict()
         for u, v, key in topo_graph.edges(keys=True):
             i_h = next(self.counter)
-            self.table[i_h] = HyperEdge(
-                degree=3, start=u, end=v, score=None, edges=[key]
-            )
-            hyper = (HYPER, i_h)
-            edge = (EDGE, key)
-            self.tripartite.add_edge(hyper, edge, extremity=True)
-            self.tripartite.add_edge(edge, (NODE, u))
-            self.tripartite.add_edge(edge, (NODE, v))
+            self.table[i_h] = HyperEdge(degree=3, score=None, edges=[(u, v, key)])
+            self.hyper2edge.add_edge(i_h, (u, v, key), extremity=True)
 
     def merge(self, ha: int, hb: int, node: int):
         sa = self.table[ha].edges
         sb = self.table[hb].edges
-        start_merge = self.table[ha].start
-        end_merge = self.table[hb].end
         if self.table[ha].start == node:
-            start_merge = self.table[ha].end
             sa = sa[::-1]
         if self.table[hb].end == node:
-            end_merge = self.table[hb].start
             sb = sb[::-1]
         d = max(self.table[ha].degree, self.table[hb].degree)
         s_merge = sa + sb
         h_new = next(self.counter)
-        self.table[h_new] = HyperEdge(
-            degree=d, edges=s_merge, start=start_merge, end=end_merge, score=None
-        )
+        self.table[h_new] = HyperEdge(degree=d, edges=s_merge, score=None)
         del self.table[ha]
         del self.table[hb]
-        self.tripartite.remove_node((HYPER, ha))
-        self.tripartite.remove_node((HYPER, hb))
+        self.hyper2edge.remove_node(ha)
+        self.hyper2edge.remove_node(hb)
         for edge in s_merge:
-            self.tripartite.add_edge((HYPER, h_new), (EDGE, edge))
-        self.tripartite.edges[((HYPER, h_new), (EDGE, s_merge[0]))]["extremity"] = True
-        self.tripartite.edges[((HYPER, h_new), (EDGE, s_merge[-1]))]["extremity"] = True
+            self.hyper2edge.add_edge(h_new, edge)
+        self.tripartite.edges[(h_new, s_merge[0])]["extremity"] = True
+        self.tripartite.edges[(h_new, s_merge[-1])]["extremity"] = True
 
     def split(self, h0: int, node: int):
-        def edge_is_incident(edge):
-            (NODE, node) in self.tripartite.neighbourgs((EDGE, edge))
-
         hyper = self.table[h0]
-        i_split = hyper.edges.find(edge_is_incident)
+        i_split = hyper.edges.find(lambda edge: edge[1] == node)
         sa = hyper[: i_split + 1]
         sb = hyper[i_split + 1 :]
         h_new_a = next(self.counter)
         h_new_b = next(self.counter)
-        self.table[h_new_a] = HyperEdge(
-            start=hyper.start, end=node, edges=sa, degree=hyper.degree, score=None
-        )
-        self.table[h_new_b] = HyperEdge(
-            start=node, end=hyper.end, edges=sb, degree=hyper.degree, score=None
-        )
-        self.tripartite.remove_node((HYPER, h0))
+        self.table[h_new_a] = HyperEdge(edges=sa, degree=hyper.degree, score=None)
+        self.table[h_new_b] = HyperEdge(edges=sb, degree=hyper.degree, score=None)
+        self.hyper2edge.remove_node(h0)
         for edge in sa:
-            self.tripartite.add_edge((HYPER, h_new_a), (EDGE, edge))
-        self.tripartite.edges[((HYPER, h_new_a), (EDGE, sa[0]))]["extremity"] = True
-        self.tripartite.edges[((HYPER, h_new_a), (EDGE, sa[-1]))]["extremity"] = True
-        self.tripartite.edges[((HYPER, h_new_b), (EDGE, sb[0]))]["extremity"] = True
-        self.tripartite.edges[((HYPER, h_new_b), (EDGE, sb[-1]))]["extremity"] = True
+            self.hyper2edge.add_edge((HYPER, h_new_a), (EDGE, edge))
+        self.hyper2edge.edges[(h_new_a, sa[0])]["extremity"] = True
+        self.hyper2edge.edges[(h_new_a, sa[-1])]["extremity"] = True
+        self.hyper2edge.edges[(h_new_b, sb[0])]["extremity"] = True
+        self.hyper2edge.edges[(h_new_b, sb[-1])]["extremity"] = True
         for edge in sb:
             self.tripartite.add_edge((HYPER, h_new_b), (EDGE, edge))
 
-    # def increase_degree(self, i: int):
-    #     d = self.data[i, "degree"]
-    #     assert d < 3
-    #     self.data[i, "degree"] = d + 1
-    #     self.data[i, "score"] = None
+    def increase_degree(self, i: int):
+        d = self.table[i].degree
+        assert d < 3
+        self.table[i].degree = d + 1
+        self.table[i].score = None
 
-    # def decrease_degree(self, i: int):
-    #     d = self.data[i, "degree"]
-    #     assert d > 2
-    #     self.data[i, "degree"] = d - 1
-    #     self.data[i, "score"] = None
+    def decrease_degree(self, i: int):
+        d = self.table[i].degree
+        assert d > 2
+        self.table[i].degree = d - 1
+        self.table[i].score = None
 
     # def random_merge(self):
     #     """
