deeaf6e5f4488e465a6c2a12215bca2f6811fe0d
rambip <apero1808@gmail.com>
2025-06-17 18:46:22 +0200

    part on perturbation completed. Now, need to undo the operations.

--- src/bez/hypergraph.py
+++ src/bez/hypergraph.py
@@ -2,6 +2,8 @@ import networkx as nx
 from dataclasses import dataclass
 import itertools
 import numpy as np
+from collections import Counter
+import random
 
 INCREASE_DEGREE = 0
 DECREASE_DEGREE = 1
@@ -9,6 +11,8 @@ MERGE = 2
 SPLIT = 3
 OVERLAP = 4
 DISSOCIATE = 5
+
+SOURCE = "*"
 
 CHOICE_DISTRIBUTION = [
     # change degree
@@ -27,137 +31,166 @@ EDGE = 1
 NODE = 2
 
 
-@dataclass
+#@dataclass
 class HyperEdge:
+    edges: list[tuple[int, int, int]]
     degree: int
     score: float | None
-    edges: list[int]
+
+    def __init__(self, edges, degree, score):
+        self.edges = edges
+        self.degree = degree
+        self.score = score
+
+    def reverse(self):
+        self.edges = [(v, u, k) for (u, v, k) in reversed(self.edges)]
+
+    def __repr__(self):
+        tokens = []
+        tokens.append(f"{self.edges[0][0]}")
+        print(self.edges)
+        for (u, v, k) in self.edges:
+            tokens.append(f"- [{k}] -> {v}")
+        tokens.append(f"( degree = {self.degree})")
+        return " ".join(tokens)
+
+class SampleError(BaseException):
+    ...
 
 
 class HyperGraph:
     def __init__(self, topo_graph: nx.MultiGraph):
-        self.counter = itertools.count(start=0)
-        self.hyper2edge = nx.DiGraph()
+        self.g = nx.DiGraph()
         self.topo = topo_graph
-        self.table = dict()
         for u, v, key in topo_graph.edges(keys=True):
-            i_h = next(self.counter)
-            self.table[i_h] = HyperEdge(degree=3, score=None, edges=[(u, v, key)])
-            self.hyper2edge.add_edge(i_h, (u, v, key), extremity=True)
+            h = HyperEdge(degree=3, score=None, edges=[(u, v, key)])
+            self.g.add_edge(h, u)
+            self.g.add_edge(h, v)
+            self.g.add_edge(SOURCE, h)
 
-    def add_hyperedge(self, degree: int, edges: list[int]):
-        i_new = next(self.counter)
-        self.table[i_new] = HyperEdge(degree=degree, edges=edges, score=None)
-        for edge in edges:
-            self.hyper2edge.add_edge(i_new, edge)
-        self.hyper2edges.edges[(i_new, edges[0])]["extremity"] = True
-        self.hyper2edges.edges[(i_new, edges[-1])]["extremity"] = True
 
-    def merge(self, ha: int, hb: int, node: int):
-        hyper_a = self.table[ha]
-        hyper_b = self.table[hb]
-        if self.table[ha].edges[-1][1] != node:
-            hyper_a.edges.reverse()
-        if self.table[hb].edges[0][0] != node:
-            hyper_b.edges.reverse()
-        d = max(self.table[ha].degree, self.table[hb].degree)
-        s_merge = hyper_a.edges + hyper_b.edges
-        self.add_hyperedge(degree=d, edges=s_merge)
+    def add_hyperedge(self, edges: list[tuple[int, int, int]], degree: int):
+        h = HyperEdge(degree=degree, edges=edges, score=None)
+        self.g.add_edge(SOURCE, h)
+        for (u, v, k) in edges:
+            self.g.add_edge(h, u)
+            self.g.add_edge(h, v)
 
-        del self.table[ha]
-        del self.table[hb]
-        self.hyper2edge.remove_node(ha)
-        self.hyper2edge.remove_node(hb)
+    def merge(self, a: HyperEdge, b: HyperEdge, node: int):
+        if a.edges[-1][1] != node:
+            a.reverse()
+        if b.edges[0][0] != node:
+            b.reverse()
+        d = max(a.degree, b.degree)
+        s_merge = a.edges + b.edges
+        self.add_hyperedge(edges=s_merge, degree=d)
 
-    def split(self, h0: int, node: int):
-        hyper = self.table[h0]
-        i_split = [i for i, edge in enumerate(hyper.edges) if edge[1] == node][0]
-        sa = hyper.edges[: i_split + 1]
-        sb = hyper.edges[i_split + 1 :]
-        self.add_hyperedge(degree=hyper.degree, edges=sa)
-        self.add_hyperedge(degree=hyper.degree, edges=sb)
-        self.hyper2edge.remove_node(h0)
-        del self.table[h0]
+        self.g.remove_node(a)
+        self.g.remove_node(b)
 
-    def overlap(self, ha, hb, edge):
+    def split(self, h: HyperEdge, node: int):
+        i_split = [i for i, edge in enumerate(h.edges) if edge[1] == node][0]
+        sa = h.edges[: i_split + 1]
+        sb = h.edges[i_split + 1 :]
+        self.add_hyperedge(degree=h.degree, edges=sa)
+        self.add_hyperedge(degree=h.degree, edges=sb)
+        self.g.remove_node(h)
+
+    def overlap(self, a: HyperEdge, b: HyperEdge, edge):
         """
         we suppose that hyper-edge `ha` starts or ends with u
         where (u, v) = edge
         """
         (u, v, k) = edge
-        hyper_a = self.table[ha]
-        _hyper_b = self.table[hb]
-        if hyper_a.edges[-1][1] != u:
-            hyper_a.edges.reverse()
-        hyper_a.edges.append(edge)
-        self.add_hyperedge(degree=hyper_a.degree, edges=hyper_a.edges)
-        self.hyper2edge.remove_node(ha)
-        del self.table[ha]
+        if a.edges[-1][1] != u:
+            a.reverse()
+        a.edges.append(edge)
+        # we remove it and add it again to update the "hyperedge -> node" information
+        self.add_hyperedge(edges=a.edges, degree=a.degree)
+        self.g.remove_node(a)
 
-    def dissociate(self, ha, hb, edge):
+    def dissociate(self, a: HyperEdge, b: HyperEdge, edge):
         """
         We suppose that the hyper-edge starts with (v, u) or ends with (u, v)
         where (u, v) = edge
         """
         (u, v, k) = edge
-        hyper_a = self.table[ha]
-        hyper_b = self.table[hb]
-        assert (u, v, k) in hyper_b.edges or (v, u, k) in hyper_b.edges
-        if hyper_a.edges[-1] != (u, v, k):
-            hyper_a.edges.reverse()
-        hyper_a.edges.pop()
-        self.add_hyperedge(degree=hyper_a.degree, edges=hyper_a.edges)
-        self.hyper2edge.remove_node(ha)
-        del self.table[ha]
+        assert (u, v, k) in a.edges or (v, u, k) in b.edges
+        if a.edges[-1] != (u, v, k):
+            a.reverse()
+        a.edges.pop()
+        self.add_hyperedge(edges=a.edges, degree=a.degree)
+        self.g.remove_node(a)
 
-    def increase_degree(self, i: int):
-        d = self.table[i].degree
+    def sample_t(self):
+        a = np.random.choice(self.g.succ[SOURCE])
+        x, y = [a.edges[0][0], a.edges[-1][1]]
+        if self.g.degree[x] > self.g.degree[y]:
+            node = x
+        else:
+            node = y
+        b = np.random.choice(self.g.pred[node])
+        if a == b:
+            raise SampleError("a = b in sample")
+        return (a, b, node)
+
+    def increase_degree(self, h: HyperEdge):
+        d = h.degree
         assert d < 3
-        self.table[i].degree = d + 1
-        self.table[i].score = None
+        h.degree = d + 1
+        h.score = None
 
-    def decrease_degree(self, i: int):
-        d = self.table[i].degree
+    def decrease_degree(self, h: HyperEdge):
+        d = h.degree
         assert d > 2
-        self.table[i].degree = d - 1
-        self.table[i].score = None
+        h.degree = d - 1
+        h.score = None
 
-    def random_merge(self):
-        """
-        To do a random merge, we need to find one node A such that there are 2 hyperedges with extremity A
-        """
+    def score(self):
+        ...
 
-    def random_split(self):
-        candidates = [i for (i, h) in self.table if len(h.edges) >= 2]
-        i = np.random.choice(candidates)
-        node = np.random.choice([u for (u, v, k) in self.table[i].edges[1:]])
-        self.split(i, node)
+    def do_perturbation(self, action):
+        ...
 
-    def random_overlap(self):
-        """
-        To do a ranodm overlap, we need to find 2 hyperedges U, V such that U is completely contained in V
-        """
+    def undo_perturbation(self, action):
+        ...
 
-    def random_dissociate(self):
-        """
-        To do a random dissociation, we need to find 2 hyperedges such that U is completely contained in V
-        """
+    def try_get_random_perturbation(self):
+        choice = np.random.choice(range(6), p=CHOICE_DISTRIBUTION)
+        a, b, node = self.sample_t()
+        if choice == INCREASE_DEGREE:
+            if a.degree >= 3:
+                raise SampleError(f"degree too high to increase\n a={a}")
+            return (choice, a)
+        if choice == DECREASE_DEGREE:
+            if a.degree <= 1:
+                raise SampleError(f"Degree too low to decrease\na={a}")
+            return (choice, a)
+        if choice == OVERLAP:
+            candidates = [edge for edge in b.edges if node in edge]
+            # FIXME: np.random.choice does not work
+            edge = random.choice(candidates)
+            return (choice, a, b, edge)
+        if choice == DISSOCIATE:
+            if a.edges[0][0] != node:
+                a.reverse()
+            (u, v, k) = a.edges[0]
+            if (u, v, k) not in b.edges and (v, u, k) not in b.edges:
+                raise SampleError(f"hypereges do not share an edge for dissociate\na={a}\nb={b}")
+            return (choice, a, b, (u, v, k))
+        if choice == MERGE:
+            if b.edges[0][0] != node and b.edges[-1][1] != node:
+                raise SampleError(f"hyperedges do not share an extremity for merge\na={a}\nb={b}\nnode={node}")
+            return (choice, a, b, node)
+        if choice == SPLIT:
+            if len(b.edges) < 3:
+                    raise SampleError(f"hyperedge is too short for split\nb={b}")
+            if b.edges[0][0] == node or b.edges[-1][1] == node:
+                raise SampleError(f"node is not at extremity for split\nb={b}")
+            return (choice, b, node)
 
     def random_perturbation(self):
-        choice = np.random.choice(range(6), p=CHOICE_DISTRIBUTION)
-        if choice == INCREASE_DEGREE:
-            candidates = [i for i, h in self.table.items() if h.degree < 3]
-            i_h = np.random.choice(candidates)
-            self.increase_degree(i_h)
-        if choice == DECREASE_DEGREE:
-            candidates = [i for i, h in self.table.items() if h.degree > 1]
-            i_h = np.random.choice(candidates)
-            self.decrease_degree(i_h)
-        if choice == OVERLAP:
-            self.random_overlap()
-        if choice == DISSOCIATE:
-            self.random_dissociate()
-        if choice == MERGE:
-            self.random_merge()
-        if choice == SPLIT:
-            self.random_split()
+        while True:
+            try:
+                self.random_perturbation()
+                return
