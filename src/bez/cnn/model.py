4d7e42569dca140841575913f4794e9fbcd335d0
Antonin Peronnet <apero1808@gmail.com>
2026-02-12 21:34:19 +0000

    add batch normalisation and compute receptive field

--- src/bez/cnn/model.py
+++ src/bez/cnn/model.py
@@ -1,33 +1,40 @@
 import torch
 
 
-class Rasterizer(torch.nn.Module):
+class SketchDenoiser(torch.nn.Module):
     def __init__(self, d_model):
         super().__init__()
-        self.conv1 = torch.nn.Conv2d(1, d_model, 5, padding=2)
-        self.reg1 = torch.nn.ReLU()
+        high_dim = d_model * 2
+        low_dim = d_model
 
-        self.conv2 = torch.nn.Conv2d(d_model, d_model, 5, dilation=3, padding=6)
-        self.reg2 = torch.nn.ReLU()
+        # Stem
+        self.stem = torch.nn.Conv2d(1, high_dim, 5, padding=2)
+        self.bn_stem = torch.nn.BatchNorm2d(high_dim)  # Add BN here
 
-        self.conv3 = torch.nn.Conv2d(d_model, d_model, 5, dilation=8, padding=16)
-        self.reg3 = torch.nn.ReLU()
+        # Convolutions
+        self.conv2 = torch.nn.Conv2d(high_dim, low_dim, 5, dilation=3, padding=6)
+        self.bn2 = torch.nn.BatchNorm2d(low_dim)
 
-        self.conv4 = torch.nn.Conv2d(d_model, d_model, 5, dilation=20, padding=40)
-        self.reg4 = torch.nn.ReLU()
+        self.conv3 = torch.nn.Conv2d(low_dim, low_dim, 5, dilation=3, padding=6)
+        self.bn3 = torch.nn.BatchNorm2d(low_dim)
 
-        # self.conv3 = torch.nn.Conv2d(d_model, 1, 5, dilation=10, padding=20)
-        self.mlp = torch.nn.Conv2d(d_model, 1, 1)
+        self.conv4 = torch.nn.Conv2d(low_dim, low_dim, 5, dilation=3, padding=6)
+        self.bn4 = torch.nn.BatchNorm2d(low_dim)
+
+        self.conv5 = torch.nn.Conv2d(low_dim, high_dim, 5, dilation=3, padding=6)
+        self.bn5 = torch.nn.BatchNorm2d(high_dim)
+
+        self.reg = torch.nn.SiLU()
+        self.mlp = torch.nn.Conv2d(high_dim, 1, 1)
         self.final = torch.nn.Sigmoid()
 
-        # self.mlp = torch.nn.Conv2d(d_model, 1, 1)
-        # self.final = torch.nn.Sigmoid()
-
     def forward(self, x):
-        x = self.reg1(self.conv1(x))
-        residual = x
-        x = self.reg2(self.conv2(x))
-        x = self.reg3(self.conv3(x) + residual)
-        x = self.reg4(self.conv4(x))
-        x = self.final(self.mlp(x))
-        return x
+        x = self.reg(self.bn_stem(self.stem(x)))
+
+        res = x
+        x = self.reg(self.bn2(self.conv2(x)))
+        x = self.reg(self.bn3(self.conv3(x)))
+        x = self.reg(self.bn4(self.conv4(x)))
+        x = self.reg(self.bn5(self.conv5(x)))
+
+        return self.final(self.mlp(x + res))
